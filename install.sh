2688b420d1a9a65fb1cf8e56fbb0739a1d2f2221
chrissx <chrissx@chrissx.de>
2021-02-27 19:41:01 +0100

    a simple and bad install script

---
+++ install.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+mkdir -p ~/.bin
+
+[ ! -d /usr/local/Homebrew ] && netsh -f install.sh gh://Homebrew/install
+[ ! -f /opt/local/bin/port ] && netpkg "$MACPORTS"
+
+sudo pacman -Syu
+sudo pacman -S cockpit
+sudo pacman -S yay
+sudo pacman -S brave-browser
+sudo pacman -S fakeroot
+sudo pacman -S dstat
+sudo pacman -S gimp
+sudo pacman -S imagemagick
+sudo pacman -S llvm
+sudo pacman -S clang
+sudo pacman -S dstat
+sudo pacman -S jdk8-openjdk
+sudo pacman -S jdk15-openjdk
+sudo pacman -S tmux
+sudo pacman -S fzf
+sudo pacman -S zsh
+sudo pacman -S git
+sudo pacman -S dosbox
+sudo pacman -S blender
+sudo pacman -S w3m
+sudo pacman -S lynx
+sudo pacman -S freecad
+sudo pacman -S kicad
+sudo pacman -S progress
+sudo pacman -S ncdu
+sudo pacman -S mpv
+sudo pacman -S obs-studio
+sudo pacman -S make
+sudo pacman -S cmake
+sudo pacman -S flex
+sudo pacman -S bison
+sudo pacman -S yubikey-manager
+sudo pacman -S vim
+sudo pacman -S emacs
+sudo pacman -S steam
+sudo pacman -S thunderbird
+sudo pacman -S feh
+sudo pacman -S rust
+sudo pacman -S iotop
+sudo pacman -S atop
+sudo pacman -S tldr
+sudo pacman -S yubioath-desktop
+sudo pacman -S yubikey-personalization-gui
+sudo pacman -S nmap
+sudo pacman -S mupdf
+sudo pacman -S zathura
+sudo pacman -S gocr
+sudo pacman -S wine
+sudo pacman -S qrencode
+sudo pacman -S unrar
+sudo pacman -S yasm
+sudo pacman -S vala
+sudo pacman -S xclip
+sudo pacman -S pandoc
+sudo pacman -S ruby
+sudo pacman -S fd
+sudo pacman -S gradle
+sudo pacman -S vbindiff
+sudo pacman -S binwalk
+sudo pacman -S texlive
+sudo pacman -S ninja
+sudo pacman -S meson
+sudo pacman -S signal-desktop
+sudo pacman -S discord
+sudo pacman -S dropbear
+sudo pacman -S dart
+sudo pacman -S flutter
+
+yay -S davinci-resolve-studio
+yay -S spotify
+yay -S 1password
+yay -S ftba
+yay -S feed-the-beast-classic
+yay -S imlib2-heic
+yay -S pfetch
+yay -S neofetch
+yay -S brew
+yay -S flashprint
+yay -S arduino
+yay -S ghidra
+
+sudo systemctl enable --now dropbear
+
+#
+# configuration
+#
+
+cp -f zshrc "$HOME/.zshrc"
+cp -f vimrc "$HOME/.vimrc"

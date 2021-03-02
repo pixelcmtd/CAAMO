445779cc7155ba5db438b2edc4ab753a3398f212
chrissx <chrissx@chrissx.de>
2021-03-02 08:54:43 +0100

    lets install vim plugins too

--- install.sh
+++ install.sh
@@ -88,5 +88,18 @@ sudo systemctl enable --now dropbear
 # configuration
 #
 
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
+git clone https://github.com/w0rp/ale ~/.vim/bundle/ale
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+git clone https://github.com/tweekmonster/gofmt.vim.git ~/.vim/bundle/gofmt.vim
+ret="$(pwd)"
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py $I3B_YCM_FLAGS
+cd "$ret"
+
 cp -f zshrc "$HOME/.zshrc"
+cp -f zshtheme "$HOME/.zshtheme"
 cp -f vimrc "$HOME/.vimrc"
